b8014886808a032f36f9435a5cc2f2f030c4a154
dkimitsa <demyan.kimitsa@gmail.com>
2020-08-18 00:39:32 +0300

    * basic Firebase/DynamicLinks -- integration test

---
+++ firebase-dylinks/src/main/java/io/github/dkimitsa/tests/DemoFirebaseAuth.java
@@ -0,0 +1,25 @@
+package io.github.dkimitsa.tests;
+
+
+import org.robovm.pods.firebase.core.FIRApp;
+
+public class DemoFirebaseAuth {
+
+    // pre-requirements:
+    // frameworks required:
+    // core:
+    //   - FirebaseCore.framework
+    //   - GoogleUtilities.framework
+    //   - PromisesObjC.framework
+
+    // goals:
+    // - check that pod is able to link/run with dependencies
+    static {
+        // configure once
+        FIRApp.configure();
+    }
+
+    public static void demo() {
+        System.out.println("demo");
+    }
+}
