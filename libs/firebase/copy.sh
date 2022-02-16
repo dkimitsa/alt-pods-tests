6983f7dc28134511533364e6ab327e67f108c289
dkimitsa <demyan.kimitsa@gmail.com>
2022-02-16 19:59:39 +0200

    * cleanups: - removed duplicate firebase-mobile-ads - fixed plugin version in firebase-google-ads - firebase copy.sh script updated to be able to pick both sim or device libs

--- libs/firebase/copy.sh
+++ libs/firebase/copy.sh
@@ -1,52 +1,63 @@
 #!/bin/sh
 
 set -e
-rm -rf core && mkdir core
 
+if [ $1 == "sim" ]
+then
+  echo "Copying SIM libs"
+  export SIM_OR_DEVICE=ios-arm64_i386_x86_64-simulator
+  export SIM_OR_DEVICE_GAD=ios-arm64_x86_64-simulator
+else
+  echo "Copying DEVICE libs"
+  export SIM_OR_DEVICE=ios-arm64_armv7
+  export SIM_OR_DEVICE_GAD=ios-arm64_armv7
+fi
+
+rm -rf core && mkdir core
 
 #
 # Core
 #
 rm -rf core && mkdir core
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseCore.xcframework/ios-arm64_i386_x86_64-simulator/FirebaseCore.framework core/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework/ios-arm64_i386_x86_64-simulator/GoogleUtilities.framework core/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/PromisesObjC.xcframework/ios-arm64_i386_x86_64-simulator/PromisesObjC.framework core/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseCore.xcframework/${SIM_OR_DEVICE}/FirebaseCore.framework core/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework/${SIM_OR_DEVICE}/GoogleUtilities.framework core/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/PromisesObjC.xcframework/${SIM_OR_DEVICE}/PromisesObjC.framework core/
 
 #
 # Analytics
 #
 rm -rf analytics && mkdir analytics
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseAnalytics.xcframework/ios-arm64_i386_x86_64-simulator analytics/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator/GoogleAppMeasurement.framework gad/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework/ios-arm64_i386_x86_64-simulator/FirebaseInstallations.framework analytics/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/ios-arm64_i386_x86_64-simulator/nanopb.framework analytics/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseAnalytics.xcframework/${SIM_OR_DEVICE} analytics/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework/${SIM_OR_DEVICE}/GoogleAppMeasurement.framework gad/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework/${SIM_OR_DEVICE}/FirebaseInstallations.framework analytics/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/${SIM_OR_DEVICE}/nanopb.framework analytics/
 
 #
 # Auth
 #
 rm -rf auth && mkdir auth
-cp -R ~/Downloads/Firebase/FirebaseAuth/FirebaseAuth.xcframework/ios-arm64_i386_x86_64-simulator/FirebaseAuth.framework auth/
-cp -R ~/Downloads/Firebase/FirebaseAuth/GTMSessionFetcher.xcframework/ios-arm64_i386_x86_64-simulator/GTMSessionFetcher.framework auth/
+cp -R ~/Downloads/Firebase/FirebaseAuth/FirebaseAuth.xcframework/${SIM_OR_DEVICE}/FirebaseAuth.framework auth/
+cp -R ~/Downloads/Firebase/FirebaseAuth/GTMSessionFetcher.xcframework/${SIM_OR_DEVICE}/GTMSessionFetcher.framework auth/
 
 #
 # Firestore
 #
 rm -rf firestore && mkdir firestore
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/ios-arm64_i386_x86_64-simulator/nanopb.framework firestore/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/${SIM_OR_DEVICE}/nanopb.framework firestore/
 
 #
 # Storage
 #
 rm -rf storage && mkdir storage
-cp -R ~/Downloads/Firebase/FirebaseStorage/FirebaseStorage.xcframework/ios-arm64_i386_x86_64-simulator/FirebaseStorage.framework storage/
-cp -R ~/Downloads/Firebase/FirebaseStorage/GTMSessionFetcher.xcframework/ios-arm64_i386_x86_64-simulator/GTMSessionFetcher.framework storage/
+cp -R ~/Downloads/Firebase/FirebaseStorage/FirebaseStorage.xcframework/${SIM_OR_DEVICE}/FirebaseStorage.framework storage/
+cp -R ~/Downloads/Firebase/FirebaseStorage/GTMSessionFetcher.xcframework/${SIM_OR_DEVICE}/GTMSessionFetcher.framework storage/
 
 #
 # Google Mobile Ads
 #
 rm -rf gad && mkdir gad
-cp -R ~/Downloads/Firebase/Google-Mobile-Ads-SDK/GoogleMobileAds.xcframework/ios-arm64_x86_64-simulator/GoogleMobileAds.framework gad/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework/ios-arm64_i386_x86_64-simulator/GoogleAppMeasurement.framework gad/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/ios-arm64_i386_x86_64-simulator/nanopb.framework gad/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework/ios-arm64_i386_x86_64-simulator/GoogleUtilities.framework gad/
-cp -R ~/Downloads/Firebase/FirebaseAnalytics/PromisesObjC.xcframework/ios-arm64_i386_x86_64-simulator/PromisesObjC.framework gad/
+cp -R ~/Downloads/Firebase/Google-Mobile-Ads-SDK/GoogleMobileAds.xcframework/${SIM_OR_DEVICE_GAD}/GoogleMobileAds.framework gad/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework/${SIM_OR_DEVICE}/GoogleAppMeasurement.framework gad/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework/${SIM_OR_DEVICE}/nanopb.framework gad/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework/${SIM_OR_DEVICE}/GoogleUtilities.framework gad/
+cp -R ~/Downloads/Firebase/FirebaseAnalytics/PromisesObjC.xcframework/${SIM_OR_DEVICE}/PromisesObjC.framework gad/
