036c30369c0f54314df1aa802d3eab95df133ff3
dkimitsa <demyan.kimitsa@gmail.com>
2024-03-09 13:25:18 +0200

    * synchronized with altpodsVersion '1.38.0-SNAPSHOT'

--- libs/copy_firebase_libs.sh
+++ libs/copy_firebase_libs.sh
@@ -17,47 +17,51 @@ rm -rf "firebase" && mkdir -p "firebase"
 #
 # Core
 #
+copy ~/Downloads/Firebase/FirebaseAnalytics/FBLPromises.xcframework core
 copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseCore.xcframework core
 copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseCoreInternal.xcframework core
+copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework core
+copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework core
+copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurementIdentitySupport.xcframework core
 copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework core
-copy ~/Downloads/Firebase/FirebaseAnalytics/FBLPromises.xcframework core
+copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework core
 
 #
 # Analytics
 #
 copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseAnalytics.xcframework analytics
-copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework analytics
-copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework analytics
-copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework analytics
 
 #
 # Auth
 #
+copy ~/Downloads/Firebase/FirebaseAuth/FirebaseAppCheckInterop.xcframework auth
 copy ~/Downloads/Firebase/FirebaseAuth/FirebaseAuth.xcframework auth
 copy ~/Downloads/Firebase/FirebaseAuth/GTMSessionFetcher.xcframework auth
-copy ~/Downloads/Firebase/FirebaseAuth/FirebaseAppCheckInterop.xcframework auth
 copy ~/Downloads/Firebase/FirebaseAuth/RecaptchaInterop.xcframework auth
 
 #
 # AppCheck
 #
+copy ~/Downloads/Firebase/FirebaseAppCheck/AppCheckCore.xcframework appcheck
 copy ~/Downloads/Firebase/FirebaseAppCheck/FirebaseAppCheck.xcframework appcheck
 copy ~/Downloads/Firebase/FirebaseAppCheck/FirebaseAppCheckInterop.xcframework appcheck
+
 #
 # Crashlytics
 #
+copy ~/Downloads/Firebase/FirebaseCrashlytics/FirebaseCoreExtension.xcframework crashlytics
 copy ~/Downloads/Firebase/FirebaseCrashlytics/FirebaseCrashlytics.xcframework crashlytics
-copy ~/Downloads/Firebase/FirebaseCrashlytics/GoogleDataTransport.xcframework crashlytics
 copy ~/Downloads/Firebase/FirebaseCrashlytics/FirebaseSessions.xcframework crashlytics
-copy ~/Downloads/Firebase/FirebaseCrashlytics/PromisesSwift.xcframework crashlytics
-copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework crashlytics
-copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework crashlytics
+copy ~/Downloads/Firebase/FirebaseCrashlytics/GoogleDataTransport.xcframework crashlytics
+copy ~/Downloads/Firebase/FirebaseCrashlytics/Promises.xcframework crashlytics
 
 #
 # FirebaseDatabase
 #
+copy ~/Downloads/Firebase/FirebaseDatabase/FirebaseAppCheckInterop.xcframework db
 copy ~/Downloads/Firebase/FirebaseDatabase/FirebaseDatabase.xcframework db
-copy ~/Downloads/Firebase/FirebaseDatabase/leveldb-library.xcframework db
+copy ~/Downloads/Firebase/FirebaseDatabase/FirebaseSharedSwift.xcframework db
+copy ~/Downloads/Firebase/FirebaseDatabase/leveldb.xcframework db
 
 #
 # FirebaseDynamicLinks
@@ -67,13 +71,16 @@ copy ~/Downloads/Firebase/FirebaseDynamicLinks/FirebaseDynamicLinks.xcframework 
 #
 # Firestore
 #
-copy ~/Downloads/Firebase/FirebaseFirestore/abseil.xcframework firestore
-copy ~/Downloads/Firebase/FirebaseFirestore/BoringSSL-GRPC.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/FirebaseAppCheckInterop.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/FirebaseCoreExtension.xcframework firestore
 copy ~/Downloads/Firebase/FirebaseFirestore/FirebaseFirestore.xcframework firestore
-copy ~/Downloads/Firebase/FirebaseFirestore/gRPC-C++.xcframework firestore
-copy ~/Downloads/Firebase/FirebaseFirestore/gRPC-Core.xcframework firestore
-copy ~//Downloads/Firebase/FirebaseFirestore/leveldb-library.xcframework firestore
-copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/FirebaseFirestoreInternal.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/FirebaseSharedSwift.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/absl.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/grpc.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/grpcpp.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/leveldb.xcframework firestore
+copy ~/Downloads/Firebase/FirebaseFirestore/openssl_grpc.xcframework firestore
 
 
 #
@@ -81,18 +88,14 @@ copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework firestore
 #
 copy ~/Downloads/Firebase/Google-Mobile-Ads-SDK/GoogleMobileAds.xcframework gad
 copy ~/Downloads/Firebase/Google-Mobile-Ads-SDK/UserMessagingPlatform.xcframework gad
-copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleAppMeasurement.xcframework gad
-copy ~/Downloads/Firebase/FirebaseAnalytics/GoogleUtilities.xcframework gad
-copy ~/Downloads/Firebase/FirebaseAnalytics/FBLPromises.xcframework gad
-copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework gad
 
 #
 # Google SignIn
 #
-copy ~/Downloads/Firebase/GoogleSignIn/GoogleSignIn.xcframework signin
 copy ~/Downloads/Firebase/GoogleSignIn/AppAuth.xcframework signin
 copy ~/Downloads/Firebase/GoogleSignIn/GTMAppAuth.xcframework signin
 copy ~/Downloads/Firebase/GoogleSignIn/GTMSessionFetcher.xcframework signin
+copy ~/Downloads/Firebase/GoogleSignIn/GoogleSignIn.xcframework signin
 
 #
 # UserMessagingPlatform
@@ -109,15 +112,13 @@ copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework in
 #
 copy ~/Downloads/Firebase/FirebaseMessaging/FirebaseMessaging.xcframework messaging
 copy ~/Downloads/Firebase/FirebaseMessaging/GoogleDataTransport.xcframework messaging
-copy ~/Downloads/Firebase/FirebaseAnalytics/nanopb.xcframework messaging
-copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework messaging
 
 #
 # FirebaseRemoteConfig
 #
 copy ~/Downloads/Firebase/FirebaseRemoteConfig/FirebaseABTesting.xcframework remoteconfig
 copy ~/Downloads/Firebase/FirebaseRemoteConfig/FirebaseRemoteConfig.xcframework remoteconfig
-copy ~/Downloads/Firebase/FirebaseAnalytics/FirebaseInstallations.xcframework remoteconfig
+copy ~/Downloads/Firebase/FirebaseRemoteConfig/FirebaseSharedSwift.xcframework remoteconfig
 
 #
 # Storage
@@ -127,5 +128,4 @@ copy ~/Downloads/Firebase/FirebaseStorage/FirebaseAuthInterop.xcframework storag
 copy ~/Downloads/Firebase/FirebaseStorage/FirebaseCoreExtension.xcframework storage
 copy ~/Downloads/Firebase/FirebaseStorage/FirebaseStorage.xcframework storage
 copy ~/Downloads/Firebase/FirebaseStorage/GTMSessionFetcher.xcframework storage
-copy ~/Downloads/Firebase/FirebaseStorage/FirebaseStorage.xcframework storage
 
